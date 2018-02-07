7e5cf6f2ed9040fa444e268be987dddb521b10cc
최희성 <mono@fscom.kr>
2018-02-07 13:03:59 +0900

    daily update

--- src/chap3/3.6.kt
+++ src/chap3/3.6.kt
@@ -2,27 +2,49 @@ package chap3
 
 class User(val id: Int, val name: String, val address: String)
 
+
+fun User.validateBeforeSave() {
+    fun validate(value: String, fieldName: String) {
+        if (value.isEmpty()) {
+            throw IllegalArgumentException( "Can't save user ${id}: empty ${fieldName}" )
+        }
+    }
+
+    validate(name, "Name")
+    validate(address, "Address")
+}
+
+
+
+
 fun saveUser(user: User) {
 
-    fun validate(user: User, value: String, fieldName: String) {
+    user.validateBeforeSave()
+
+
+    /*
+    fun validate(value: String, fieldName: String) {
         if (value.isEmpty()) {
             throw IllegalArgumentException( "Can't save user ${user.id}: empty ${fieldName}" )
         }
     }
 
-    validate(user, user.name, "Name")
-    validate(user, user.address, "Address")
-
-
-    /*
-    if (user.name.isEmpty()) {
-        throw IllegalArgumentException( "Can't save user ${user.id}: empty Name" )
-    }
-
-    if (user.address.isEmpty()) {
-        throw IllegalArgumentException( "Can't save user ${user.id}: empty Address" )
-    }
+    validate(user.name, "Name")
+    validate(user.address, "Address")
     */
+
+
+
+//    if (user.name.isEmpty()) {
+//        throw IllegalArgumentException( "Can't save user ${user.id}: empty Name" )
+//    }
+//
+//    if (user.address.isEmpty()) {
+//        throw IllegalArgumentException( "Can't save user ${user.id}: empty Address" )
+//    }
+
+
+
 }
 
 fun main(args: Array<String>) {
